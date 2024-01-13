534bd7b5df555956f0ab4bc620cf569b24d1a351
Npx <cheyprivado@gmail.com>
2024-01-13 17:49:07 -0300

    ✨ Adicionando atualização de dados do user

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,15 +1,43 @@
+import { useState } from "react";
 import "./style.css";
 
 export function App() {
+  const [userName, setUserName] = useState("");
+
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  function handleUpdateUserInfo(avatar, name, bio) {
+    document.querySelector(".user-avatar").src = avatar;
+    document.querySelector(".user-name").innerText = name;
+    document.querySelector(".user-bio").innerText = bio;
+  }
+
+  const handleGetUserInfo = (name) => {
+    fetch(`https://api.github.com/users/${name}`)
+      .then((response) => response.json())
+      .then((data) => {
+        handleUpdateUserInfo(data.avatar_url, data.name, data.bio);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    handleGetUserInfo(userName);
+  };
+
   return (
     <div className="container">
       <header>
         <h1>Github Info Picker</h1>
-        <form action="#">
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Digite o nome do usuário..."
             required
+            onChange={handleUserNameChange}
           />
           <button type="submit">Pegar informações</button>
         </form>
