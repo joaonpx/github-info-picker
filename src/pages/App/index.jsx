be78a3ba94a3efb1ddeeb3d012703a78376cee78
Npx <cheyprivado@gmail.com>
2024-01-14 17:26:42 -0300

    💄 Atualizando UI

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,56 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 
 export function App() {
-  const [userName, setUserName] = useState("");
+  const [name, setName] = useState("");
+  const [user, setUser] = useState({ name: "", avatar: "", bio: "" });
 
-  const handleUserNameChange = (e) => {
-    setUserName(e.target.value);
-  };
+  useEffect(() => {
+    setUser({
+      name: "...",
+      avatar:
+        "https://as2.ftcdn.net/v2/jpg/03/31/69/91/1000_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg",
+      bio: "...",
+    });
+  }, []);
 
-  function handleUpdateUserInfo(avatar, name, bio) {
-    document.querySelector(".user-avatar").src = avatar;
-    document.querySelector(".user-name").innerText = name;
-    document.querySelector(".user-bio").innerText = bio;
+  function handleUserInfo() {
+    fetch(`https://api.github.com/users/${name}`).then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          setUser({
+            name: data.name,
+            avatar: data.avatar_url,
+            bio: data.bio,
+          });
+        });
+      } else {
+        setUser({
+          name: "...",
+          avatar:
+            "https://as2.ftcdn.net/v2/jpg/03/31/69/91/1000_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg",
+          bio: "...",
+        });
+      }
+    });
   }
-
-  const handleGetUserInfo = (name) => {
-    fetch(`https://api.github.com/users/${name}`)
-      .then((response) => response.json())
-      .then((data) => {
-        handleUpdateUserInfo(data.avatar_url, data.name, data.bio);
-      });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    handleGetUserInfo(userName);
-  };
 
   return (
     <div className="container">
       <header>
         <h1>Github Info Picker</h1>
-        <form onSubmit={handleSubmit}>
+        <div className="wrapper">
           <input
             type="text"
             placeholder="Digite o nome do usuário..."
             required
-            onChange={handleUserNameChange}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
-          <button type="submit">Pegar informações</button>
-        </form>
+          <button type="button" onClick={handleUserInfo}>
+            Pegar informações
+          </button>
+        </div>
       </header>
 
       <div className="user-card">
-        <img
-          className="user-avatar"
-          src="https://as2.ftcdn.net/v2/jpg/03/31/69/91/1000_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg"
-          alt="Foto de perfil"
-        />
-        <h2 className="user-name"></h2>
-        <p className="user-bio"></p>
+        <img className="user-avatar" src={user.avatar} alt="Foto de perfil" />
+        <h2 className="user-name">{user.name}</h2>
+        <p className="user-bio">{user.bio}</p>
       </div>
     </div>
   );
