6c47fe5134571b13f48e5649581196d17afecacd
Npx <cheyprivado@gmail.com>
2024-01-16 21:15:23 -0300

    ✨ Update error console message

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -31,6 +31,8 @@ export function App() {
             "https://as2.ftcdn.net/v2/jpg/03/31/69/91/1000_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg",
           bio: "...",
         });
+
+        console.log(`${response.status}: User not found`);
       }
     });
   }
